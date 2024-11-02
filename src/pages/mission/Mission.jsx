a92e3cdf2945628c890d0ff86683d5088f7a0b64
connect-beingShakti <connect.beingshakti@gmail.com>
2024-11-03 01:07:49 +0530

    adding text animation to the text

--- src/pages/mission/Mission.jsx
+++ src/pages/mission/Mission.jsx
@@ -1,13 +1,15 @@
 import "./Mission.css"
 const Mission = () => {
     const p = ""
-    return <section id="mission" className="bg-pink-100 text-slate-950 flex flex-col items-center justify-between px-12 py-12 md:py-12">
+    return <section id="mission" className="bg-slate-950">
+        <div className="mission text-slate-150 flex flex-col items-center justify-between px-12 py-12 md:py-12">
         <h1 className="text-4xl md:text-5xl font-bold mb-6">
-            Our Mission at Being Shakti
+            <span>Our Mission at Being Shakti</span> 
         </h1>
         <p className="px-2 py-12 md:py-12 text-4x1 md:text-2xl md:px-12">
-            At Being Shakti, our mission is to provide transformative practices and guidance that nurture the mind, body, and spirit. We believe that everyone has the potential to tap into their inner power, and we are here to support you on that journey. Through our services, workshops, and resources, we aim to inspire and uplift those seeking clarity, balance, and fulfillment in their lives.
+            <span>At Being Shakti, our mission is to provide transformative practices and guidance that nurture the mind, body, and spirit. We believe that everyone has the potential to tap into their inner power, and we are here to support you on that journey. Through our services, workshops, and resources, we aim to inspire and uplift those seeking clarity, balance, and fulfillment in their lives.</span>
         </p>
+        </div>
     </section>
 }
 
